f3ba6267a71b7df630ad48edb345e416a65a5cd7
Edwin Steiner <esteiner@inventage.com>
2025-10-15 20:25:22 +0200

    Add sources from https://github.com/xsuchy09/singular-keycloak-database-federation

---
+++ extensions/keycloak-database-userstorage/README.md
@@ -0,0 +1,13 @@
+Keycloak Database Userstorage
+===
+
+
+```sql
+select "id",            "username",            "email",            "firstName",            "lastName",            "cpf",            "fullName" from users where "id" = ? 
+```
+
+[build-pipeline]: https://github.com/keycloak-competence-center/keycloak-database-userstorage/actions/workflows/build-pipeline.yml
+[release-pipeline]: https://github.com/keycloak-competence-center/keycloak-database-userstorage/actions/workflows/release-pipeline.yml
+[xsuchy09]: https://github.com/xsuchy09/singular-keycloak-database-federation
+[t7tran]: https://github.com/t7tran/singular-keycloak-database-federation
+[opensingular]: https://github.com/opensingular/singular-keycloak-database-federation
---
+++ extensions/keycloak-database-userstorage/src/main/java/org/opensingular/dbuserprovider/persistence/RDBMS.java
@@ -0,0 +1,59 @@
+package org.opensingular.dbuserprovider.persistence;
+
+
+import org.hibernate.dialect.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum RDBMS {
+
+    POSTGRESQL("PostgreSQL 10+", org.postgresql.Driver.class.getName(), "SELECT 1", new PostgreSQLDialect());
+//    MYSQL("MySQL 5.7+", com.mysql.cj.jdbc.Driver.class.getName(), "SELECT 1", new MySQLDialect()),
+//    ORACLE("Oracle 12+", oracle.jdbc.OracleDriver.class.getName(), "SELECT 1 FROM DUAL", new OracleDialect()),
+//    SQL_SERVER("MS SQL Server 2012+ (jtds)", net.sourceforge.jtds.jdbc.Driver.class.getName(), "SELECT 1", new SQLServerDialect());
+
+    private final String  desc;
+    private final String  driver;
+    private final String  testString;
+    private final Dialect dialect;
+
+    RDBMS(String desc, String driver, String testString, Dialect dialect) {
+        this.desc = desc;
+        this.driver = driver;
+        this.testString = testString;
+        this.dialect = dialect;
+    }
+
+    public static RDBMS getByDescription(String desc) {
+        for (RDBMS value : values()) {
+            if (value.desc.equals(desc)) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+    public Dialect getDialect() {
+        return dialect;
+    }
+
+    public static List<String> getAllDescriptions() {
+        return Arrays.stream(values()).map(RDBMS::getDesc).collect(Collectors.toList());
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public String getTestString() {
+        return testString;
+    }
+
+
+}
